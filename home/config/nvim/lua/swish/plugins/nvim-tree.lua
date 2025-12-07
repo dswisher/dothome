752029d41bbece8c12ad8a6a9c335a363a8471bd
Doug Swisher <doug.swisher@dowjones.com>
2025-12-07 10:19:27 -0600

    Start setting up nvim-tree

---
+++ home/config/nvim/lua/swish/plugins/nvim-tree.lua
@@ -0,0 +1,38 @@
+
+-- See https://github.com/nvim-tree/nvim-tree.lua
+
+return {
+    "nvim-tree/nvim-tree.lua",
+    version = "*",
+    lazy = false,
+    dependencies = {
+        "nvim-tree/nvim-web-devicons",
+    },
+    config = function()
+        local api = require "nvim-tree.api"
+
+        require("nvim-tree").setup {
+            view = {
+                width = 30,
+                adaptive_size = true,
+            },
+            filters = {
+                dotfiles = false
+            },
+        }
+
+        -- Open or focus the explorer tree
+        -- vim.keymap.set("n", "<leader>e", "<CMD>NvimTreeFocus<CR>", { desc = "NvimTree: open or focus" })
+        -- vim.keymap.set("n", "<leader>ee", "<CMD>NvimTreeToggle<CR>", { desc = "NvimTree: toggle open" })
+        -- vim.keymap.set("n", "<leader>ee", "<CMD>NvimTreeFindFileToggle<CR>", { desc = "NvimTree: toggle open and find file" })
+        --
+        vim.keymap.set("n", "<leader>ee", "<CMD>NvimTreeOpen<CR><CMD>NvimTreeFindFile<CR>", { desc = "NvimTree: toggle open and find file" })
+        vim.keymap.set("n", "<leader>ec", "<CMD>NvimTreeClose<CR>", { desc = "NvimTree: close" })
+
+        -- TODO: use the API to do this?
+        -- NOTE: these do not work
+        -- vim.keymap.set("n", "<leader>e", api.tree.toggle({ find_file = true, update_root = false, focus = true, }), { desc = "NvimTree: toggle open" })
+        -- vim.keymap.set("n", "<leader>e", api.tree.toggle(), { desc = "NvimTree: toggle open" })
+    end,
+}
+
